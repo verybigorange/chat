655d8231fb4681ec0dae3aae838181ce098d0736
verybigorange <zhoucheng1993@foxmail.com>
2018-03-12 22:19:54 +0800

    基础搭建

--- src/views/login.js
+++ src/views/login.js
@@ -1,1 +1,20 @@
-// 登录页
+import { TextField } from 'material-ui'
+import React,{ Component } from 'react';
+
+class Login extends Component{
+    render(){
+        return (
+            <div style={{'textAlign':'center'}}>
+                <section>
+                    <TextField floatingLabelText="账号:"  name='username'></TextField>
+                </section>
+               
+                <section>
+                    <TextField floatingLabelText="密码:"  name='password'></TextField>
+                </section>
+            </div> 
+        )
+    }
+}
+
+export default Login;
