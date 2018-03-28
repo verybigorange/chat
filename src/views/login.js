cea7444b7a81e645c18931befa4c2788954b50d7
verybigorange <449732828@qq.com>
2018-03-28 16:39:11 +0800

    增加登录页面

--- src/views/login.js
+++ src/views/login.js
@@ -1,23 +1,35 @@
-import { TextField } from 'material-ui';
+import { TextField,RaisedButton } from 'material-ui';
+import 'assets/css/login.scss';
 import React,{ Component } from 'react';
 import { login } from "store/login/action";
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+
 
 @connect(state=>({userInfo:state.userInfo}),{
     login
 })
 class Login extends Component{
     render(){
-        const loginPage = ( <div style={{'textAlign':'center'}}>
-            <button onClick={this.props.login}>登录</button>
-            <section>
-                <TextField floatingLabelText="账号:"  name='username'></TextField>
-            </section>
-        
-            <section>
-                <TextField floatingLabelText="密码:"  name='password'></TextField>
-            </section>
+        const loginPage = ( <div className="login">
+           <div className="container">
+                <header className="login-head">chat即时聊天</header>
+                <section>
+                    <TextField floatingLabelText="账号:"  name='username' style={{width:'100%'}}></TextField>
+                </section>
+            
+                <section>
+                    <TextField floatingLabelText="密码:"  name='password' style={{width:'100%'}}></TextField>
+                </section>
+                <section className="mt20 login-foot">
+                    <RaisedButton className="fl" label="登录" primary={true}></RaisedButton>
+                    <div>
+                        <small className="mr10">忘记密码</small>
+                        <small>注册账号</small>
+                    </div> 
+                   
+                </section>
+           </div>
          </div> )
         return this.props.userInfo.isLogin?(<Redirect to="/dashbord"></Redirect>):loginPage
     }
