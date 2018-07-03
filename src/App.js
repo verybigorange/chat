dd76507be0700a495bc978c35f26e5d6d0dda5cf
verybigorange <zhoucheng@rsung.com>
2018-07-03 15:45:15 +0800

    组件懒加载

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter,Switch,Route,Redirect } from 'react-router-dom'
-import DashBoard from './views/DashBoard';
-import Login from './views/Login';
-import Register from './views/Register';
+import { BrowserRouter,Switch,Route,Redirect } from 'react-router-dom';
 import Loading from 'components/Loading';
 import { connect } from 'react-redux';
+import asyncComponent from './utils/asyncComponent';
+
+// 懒加载组件
+const DashBoard = asyncComponent(() => import('./views/DashBoard'));
+const Login = asyncComponent(() => import('./views/Login'));
+const Register = asyncComponent(() => import('./views/Register'));
 
 @connect(state=>({loading:state.loading}))
 class App extends Component {
--- src/utils/asyncComponent.jsx
+++ src/utils/asyncComponent.jsx
@@ -1,27 +1,36 @@
-import React, { Component } from "react";
+import React from 'react'
 
-export default function asyncComponent(importComponent) {
-  class AsyncComponent extends Component {
-    constructor(props) {
-      super(props);
+const asyncComponent = loadComponent => (
+    class AsyncComponent extends React.Component {
+        state = {
+            Component: null,
+        }
 
-      this.state = {
-        component: null
-      };
+        componentWillMount() {
+            if (this.hasLoadedComponent()) {
+                return;
+            }
+
+            loadComponent()
+                .then(module => module.default)
+                .then((Component) => {
+                    this.setState({ Component });
+                })
+                .catch((err) => {
+                    console.error(`Cannot load component in <AsyncComponent />`);
+                    throw err;
+                });
+        }
+
+        hasLoadedComponent() {
+            return this.state.Component !== null;
+        }
+
+        render() {
+            const { Component } = this.state;
+            return (Component) ? <Component {...this.props} /> : null;
+        }
     }
+);
 
-    async componentDidMount() {
-      const { default: component } = await importComponent();
-
-      this.setState({component});
-    }
-
-    render() {
-      const C = this.state.component;
-
-      return C ? <C {...this.props} /> : null;
-    }
-  }
-
-  return AsyncComponent;
-}
+export default asyncComponent
